26be9252e31a40faad9996c56bcf4d7bea2854ae
Andreas Baumann <mail@andreasbaumann.cc>
2016-05-18 18:53:30 +0200

    added OSX support

--- dist/travis/before_script.sh
+++ dist/travis/before_script.sh
@@ -1,4 +1,27 @@
 #!/bin/sh
 
-sudo apt-get update -qq
-sudo apt-get install -y cmake libboost-all-dev
+OS=$(uname -s)
+
+case $OS in
+	Linux)
+		sudo apt-get update -qq
+		sudo apt-get install -y \
+			cmake \
+			libboost-all-dev
+		;;
+		
+	Darwin)
+		# || true below to cope rc=1 when boost is already installed.
+		brew install \
+			boost \
+			cmake \
+			gettext \
+			leveldb || true
+		brew link --force gettext || true
+		;;
+	
+	default)
+		echo "ERROR: unknown operating system '$OS'."
+		;;
+esac
+
