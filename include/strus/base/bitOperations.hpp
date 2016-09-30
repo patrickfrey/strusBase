d938384fadf80679f6b88fcbff0e4e69e93b648e
patrickfrey <patrickpfrey@yahoo.com>
2016-09-30 11:14:45 +0200

    version 0.11.1: moved bit operations header file from project strus here

--- include/strus/base/bitOperations.hpp
+++ include/strus/base/bitOperations.hpp
@@ -5,9 +5,9 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-#ifndef _STRUS_STORAGE_BIT_OPERATIONS_HPP_INCLUDED
-#define _STRUS_STORAGE_BIT_OPERATIONS_HPP_INCLUDED
-#include "strus/index.hpp"
+#ifndef _STRUS_BASE_BIT_OPERATIONS_HPP_INCLUDED
+#define _STRUS_BASE_BIT_OPERATIONS_HPP_INCLUDED
+#include "strus/base/stdint.h"
 #include <cstdlib>
 #include <cstring>
 
