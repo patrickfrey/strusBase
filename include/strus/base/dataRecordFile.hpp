8257893484985629607c0c70bbf2aff3d21810b4
patrickfrey <patrickpfrey@yahoo.com>
2016-10-20 16:54:48 +0200

    reorganized files:  put data record file type into own module

---
+++ include/strus/base/dataRecordFile.hpp
@@ -0,0 +1,85 @@
+/*
+ * Copyright (c) 2014 Patrick P. Frey
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+/// \brief Basic portable file type for collections of equal sized records with random access
+#ifndef _STRUS_BASE_DATA_RECORD_FILE_HPP_INCLUDED
+#define _STRUS_BASE_DATA_RECORD_FILE_HPP_INCLUDED
+#include "strus/base/fileio.hpp"
+#include <vector>
+#include <string>
+#include <cstdio>
+
+namespace strus
+{
+
+/// \brief Basic portable file type for collections of equal sized records with random access
+#ifdef _MSC_VER
+class DataRecordFile;
+//... Posix file functions do not open a file in shared read mode in Windows.
+//    Therefore we need an own implementation with Win file functions that is not available yet.
+#else
+/// \remark Implementaion is not thread safe
+class DataRecordFile
+{
+public:
+	/// \brief Implemented modes to access a data record file
+	enum Mode {NoAccess=0x0, SharedRead=0x1, ExclusiveAppendWrite=0x4};
+
+public:
+	///\brief Constructor
+	DataRecordFile();
+	///\brief Destructor
+	~DataRecordFile();
+
+	///\brief Open a file
+	///\param[in] filename name of the file o open
+	///\param[in] mode_ file mode describing the access to the file
+	///\param[in] recordsize_ size of one record in bytes
+	///\return true, if success, false on failure (see error() for the system error code ~ errno)
+	bool open( const std::string& filename, const Mode& mode_, unsigned int recordsize_);
+
+	///\brief Read one record of the file
+	///\param[in] fpos index of the record to read (file position is the index multiplied with the record size)
+	///\return pointer to buffer read if success, (0) NULL on failure (see error() for the system error code ~ errno)
+	const void* read( std::size_t fpos);
+
+	///\brief Append a record at the end of the file
+	///\param[in] recbuf buffer containing the record to write
+	///\return true, if success, false on failure (see error() for the system error code ~ errno)
+	bool append( const void* recbuf);
+
+	///\brief Close the file
+	///\return true, if success, false on failure (see error() for the system error code ~ errno)
+	bool close();
+
+	///\brief Get the error of the last operation to this file
+	///\return the error code (system errno)
+	int error() const;
+
+	///\brief Get the size of one record of this file
+	std::size_t recordsize() const		{return m_recordsize;}
+
+	///\brief Get the number of records stored in the file
+	std::size_t size();
+
+private:
+	FILE* m_fh;			///< file handle
+	Mode m_mode;			///< file access mode
+	int m_errno;			///< system errno for this file
+	unsigned int m_recordsize;	///< size of a record in bytes
+	std::size_t m_recordindex;	///< current file position
+	void* m_recbuf;
+
+private:
+	DataRecordFile( const DataRecordFile&){}				///< non copyable
+	DataRecordFile& operator=( const DataRecordFile&){return *this;}	///< non copyable
+};
+#endif
+
+}//namespace
+#endif
+
---
+++ src/base/dataRecordFile.cpp
@@ -0,0 +1,190 @@
+/*
+ * Copyright (c) 2014 Patrick P. Frey
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+/// \brief Basic portable file type for collections of equal sized records with random access
+#include "strus/base/dataRecordFile.hpp"
+#include "strus/base/dll_tags.hpp"
+#include "fileio_sys.hpp"
+#include <cstdio>
+#include <cerrno>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <vector>
+#include <string>
+#include <cstring>
+#include <cstdlib>
+#include <limits>
+
+using namespace strus;
+
+#ifdef _MSC_VER
+class DataRecordFile {};
+#else
+DLL_PUBLIC DataRecordFile::DataRecordFile()
+	:m_fh(0),m_mode(NoAccess),m_errno(0),m_recordsize(0),m_recordindex(0),m_recbuf(0)
+{}
+
+DLL_PUBLIC DataRecordFile::~DataRecordFile()
+{
+	(void)close();
+}
+
+DLL_PUBLIC bool DataRecordFile::open( const std::string& filename, const Mode& mode_, unsigned int recordsize_)
+{
+	if (m_fh && !close()) return false;
+	switch (mode_)
+	{
+		case NoAccess:
+			m_fh = 0;
+			m_errno = 0;
+			break;
+		case SharedRead:
+			m_recbuf = std::malloc( recordsize_);
+			if (!m_recbuf)
+			{
+				m_errno = ENOMEM;
+				return false;
+			}
+			m_fh = ::fopen( filename.c_str(), "rb");
+			if (!m_fh) m_errno = errno;
+			break;
+		case ExclusiveAppendWrite:
+			m_fh = ::fopen( filename.c_str(), "ab");
+			if (!m_fh) m_errno = errno;
+			break;
+	}
+	m_mode = mode_;
+	m_recordsize = recordsize_;
+	m_recordindex = 0;
+	return (m_errno == 0);
+}
+
+DLL_PUBLIC const void* DataRecordFile::read( std::size_t fpos)
+{
+	switch (m_mode)
+	{
+		case ExclusiveAppendWrite:
+		case NoAccess:
+			break;
+		case SharedRead:
+		{
+			int origin;
+			long int offset; 
+			if (m_recordindex > fpos)
+			{
+				origin = SEEK_SET;
+				offset = fpos * m_recordsize;
+			}
+			else
+			{
+				origin = SEEK_CUR;
+				offset = (fpos - m_recordindex) * m_recordsize;
+				if (offset == 0 && ::feof( m_fh))
+				{
+					return 0;
+				}
+			}
+			if (((origin == SEEK_CUR && offset == 0) || 0==fseek( m_fh, offset, origin))
+			&& 1==fread( m_recbuf, m_recordsize, 1/*count*/, m_fh))
+			{
+				m_recordindex = fpos + 1;
+			}
+			else
+			{
+				m_errno = ferror( m_fh);
+				m_recordindex = std::numeric_limits<std::size_t>::max();
+				return 0;
+			}
+			return m_recbuf;
+		}
+	}
+	m_errno = EINVAL;
+	return 0;
+}
+
+DLL_PUBLIC bool DataRecordFile::append( const void* recbuf)
+{
+	switch (m_mode)
+	{
+		case SharedRead:
+		case NoAccess:
+			break;
+		case ExclusiveAppendWrite:
+			if (1==fwrite( recbuf, m_recordsize, 1/*count*/, m_fh))
+			{
+				++m_recordindex;
+			}
+			else
+			{
+				m_errno = ::ferror( m_fh);
+			}
+			return true;
+	}
+	m_errno = EINVAL;
+	return false;
+}
+
+DLL_PUBLIC bool DataRecordFile::close()
+{
+	if (0!=fclose( m_fh))
+	{
+		m_errno = ::ferror( m_fh);
+	}
+	else
+	{
+		m_errno = 0;
+	}
+	if (m_recbuf)
+	{
+		std::free( m_recbuf);
+		m_recbuf = 0;
+	}
+	m_fh = 0;
+	m_mode = NoAccess;
+	m_recordsize = 0;
+	m_recordindex = 0;
+	return (m_errno == 0);
+}
+
+DLL_PUBLIC int DataRecordFile::error() const
+{
+	return m_errno;
+}
+
+DLL_PUBLIC std::size_t DataRecordFile::size()
+{
+	switch (m_mode)
+	{
+		case NoAccess:
+			break;
+		case ExclusiveAppendWrite:
+			return m_recordindex;
+		case SharedRead:
+		{
+			if (0==::fseek( m_fh, 0, SEEK_END))
+			{
+				long int nn = ::ftell( m_fh);
+				if (nn < 0)
+				{
+					m_recordindex = std::numeric_limits<std::size_t>::max();
+					return 0;
+				}
+				else
+				{
+					m_recordindex = (nn / m_recordsize);
+					return m_recordindex;
+				}
+			}
+		}
+	}
+	m_errno = EINVAL;
+	return 0;
+}
+#endif
+
+
