d2bcb374eaf996e9eb181f5c88ced08703053d15
patrickfrey <patrickpfrey@yahoo.com>
2017-09-22 12:16:18 +0200

    travis build on master branch commit

--- dist/travis/script.sh
+++ dist/travis/script.sh
@@ -12,7 +12,7 @@ case $OS in
 			-DLIB_INSTALL_DIR=lib -DCMAKE_CXX_FLAGS=-g \
 			..
 		make VERBOSE=1
-		make VERBOSE=1 test
+		make VERBOSE=1 CTEST_OUTPUT_ON_FAILURE=1 test
 		sudo make VERBOSE=1 install
 		cd ..
 		;;
@@ -33,7 +33,7 @@ case $OS in
 				-DCMAKE_CXX_FLAGS=-g -G 'Unix Makefiles' \
 				..
 			make VERBOSE=1
-			make VERBOSE=1 test
+			make VERBOSE=1 CTEST_OUTPUT_ON_FAILURE=1 test
 			sudo make VERBOSE=1 install
 			cd ..
 		else
