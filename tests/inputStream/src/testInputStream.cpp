a900623350faff22d155c42f28fa6236f2935004
patrickfrey <patrickpfrey@yahoo.com>
2017-12-15 15:17:50 +0100

    fix missing include

--- tests/inputStream/src/testInputStream.cpp
+++ tests/inputStream/src/testInputStream.cpp
@@ -109,7 +109,7 @@ static void test( const std::string& testfilename, unsigned int docsize)
 					{
 						eof = true;
 					}
-					else if (error != EINVAL)
+					else if (error != 22/*EINVAL*/)
 					{
 						throw std::runtime_error(::strerror(error));
 					}
@@ -141,7 +141,7 @@ static void test( const std::string& testfilename, unsigned int docsize)
 					{
 						eof = true;
 					}
-					else if (error != EINVAL)
+					else if (error != 22/*EINVAL*/)
 					{
 						throw std::runtime_error(::strerror(error));
 					}
