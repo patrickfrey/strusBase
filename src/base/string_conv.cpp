6a15d270f225bc987301c6c25c17205cb17e2697
Andreas Baumann <mail@andreasbaumann.cc>
2017-12-16 21:22:16 +0100

    fixed stringStartsWith and caseInsensitiveStartsWith when prefix end of value is reached

--- src/base/string_conv.cpp
+++ src/base/string_conv.cpp
@@ -114,10 +114,12 @@ DLL_PUBLIC bool strus::caseInsensitiveStartsWith( const std::string& val, const 
 	if (val.size() < prefix.size()) return false;
 	std::string::const_iterator vi = val.begin(), ve = val.end();
 	std::string::const_iterator pi = prefix.begin(), pe = prefix.end();
-	for (; pi != pe; ++pi,++vi)
+	for (; pi != pe, vi != ve; ++pi,++vi)
 	{
 		if (std::tolower( *vi) != std::tolower( *pi)) return false;
 	}
+	// val smaller than prefix, this is treated as a not-match
+	if (vi == ve) return false;
 	return pi == pe;
 }
 
@@ -127,10 +129,12 @@ DLL_PUBLIC bool strus::stringStartsWith( const std::string& val, const std::stri
 	if (val.size() < prefix.size()) return false;
 	std::string::const_iterator vi = val.begin(), ve = val.end();
 	std::string::const_iterator pi = prefix.begin(), pe = prefix.end();
-	for (; pi != pe; ++pi,++vi)
+	for (; pi != pe, vi != ve; ++pi,++vi)
 	{
 		if (*vi != *pi) return false;
 	}
+	// val smaller than prefix, this is treated as a not-match
+	if (vi == ve) return false;
 	return pi == pe;
 }
 
