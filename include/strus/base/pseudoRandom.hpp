95b87d6a1bed09fde8a13807f8d5d2f8d8798ec6
patrickfrey <patrickpfrey@yahoo.com>
2018-04-12 10:36:58 +0200

    moved pseudo random number generator to strus base

---
+++ include/strus/base/pseudoRandom.hpp
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2014 Patrick P. Frey
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+#ifndef _STRUS_BASE_PSEUDO_RANDOM_NUMBERS_HPP_INCLUDED
+#define _STRUS_BASE_PSEUDO_RANDOM_NUMBERS_HPP_INCLUDED
+/// \brief Fast generator for series of pseudo random numbers where the security is not an issue and the quality of the distribution has not to be guaranteed
+/// \note Mainly used for tests with a seed depending on the current day, so that repetition on the same date produce identical series
+#include <ctime>
+#include <cstdlib>
+#include <cstdarg>
+#include <stdexcept>
+#include "strus/base/stdint.h"
+
+namespace strus
+{
+
+/// \brief Pseudo random generator. Fast generator for series of pseudo random numbers where the security is not an issue and the quality of the distribution has not to be guaranteed
+class PseudoRandom
+{
+public:
+	/// \brief Constructor with seed derived from current date (granularity day)
+	PseudoRandom();
+	/// \brief Constructor with seed passed as argument
+	explicit PseudoRandom( int seed_);
+
+	/// \brief Get the next "random" number between two numbers
+	/// \param[in] start_ minumum result
+	/// \param[in] end_ upper border of range (first number not included)
+	/// \return "randomly" selected integer in [start_,end_[
+	int get( int start_, int end_);
+
+	/// \brief Get the next "random" value of a specified set
+	/// \param[in] psize size of candidate set
+	/// \param[in] firstValue first result candidate (for ensuring that there is at least one)
+	/// \param[in] ... list of other candidates to select one from (int's)
+	/// \return "randomly" selected candidate
+	int select( unsigned int psize, int firstValue, ...);
+
+private:
+	unsigned int m_value;
+	unsigned int m_incr;
+};
+
+}//namespace
+#endif
+
+
+
---
+++ src/base/pseudoRandom.cpp
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) 2014 Patrick P. Frey
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+/// \brief Fast generator for series of pseudo random numbers where the security is not an issue and the quality of the distribution has not to be guaranteed
+/// \note Mainly used for tests with a seed depending on the current day, so that repetition on the same date produce identical series
+#include "strus/base/pseudoRandom.hpp"
+#include "strus/base/dll_tags.hpp"
+#include <ctime>
+#include <cstdlib>
+#include <cstdarg>
+#include <stdexcept>
+#include <algorithm>
+#include "strus/base/stdint.h"
+
+using namespace strus;
+
+/// \brief Pseudo random generator 
+enum {KnuthIntegerHashFactor=2654435761U};
+
+static inline uint32_t uint32_hash( uint32_t a)
+{
+	a += ~(a << 15);
+	a ^=  (a >> 10);
+	a +=  (a << 3);
+	a ^=  (a >> 6);
+	a += ~(a << 11);
+	a ^=  (a >> 16);
+	return a;
+}
+
+DLL_PUBLIC PseudoRandom::PseudoRandom()
+{
+	time_t nowtime;
+	struct tm* now;
+
+	::time( &nowtime);
+	now = ::localtime( &nowtime);
+
+	m_value = uint32_hash( ((now->tm_year+1)
+				* (now->tm_mon+100)
+				* (now->tm_mday+1)));
+	m_incr = m_value * KnuthIntegerHashFactor;
+}
+
+DLL_PUBLIC PseudoRandom::PseudoRandom( int seed_)
+{
+	m_value = seed_;
+	m_incr = m_value * KnuthIntegerHashFactor;
+}
+
+DLL_PUBLIC int PseudoRandom::get( int min_, int max_)
+{
+	if (min_ >= max_)
+	{
+		if (min_ == max_) return min_;
+		std::swap( min_, max_);
+	}
+	m_value = uint32_hash( m_value + 1 + m_incr++);
+	unsigned int iv = max_ - min_;
+	return (int)(m_value % iv) + min_;
+}
+
+DLL_PUBLIC int PseudoRandom::select( unsigned int psize, int firstValue, ...)
+{
+	va_list ap;
+	if (psize == 0) return firstValue;
+	int pidx = get( 0, psize);
+	va_start( ap, firstValue);
+	int rt = firstValue;
+	for (int ii = 0; ii < pidx; ii++)
+	{
+		rt = va_arg( ap, int);
+	}
+	va_end(ap);
+	return rt;
+}
+
